c8cdfda64a796775530dbf850dd50ba818e38158
Akash <thehellmaker@gmail.com>
2018-05-20 07:56:08 +0530

    1. Bootstrap Vue Integration with Dropdown 2. Favicon

--- src/components/CreateAPI.vue
+++ src/components/CreateAPI.vue
@@ -1,32 +1,41 @@
 <template if="PiedPiper">
+
   <div class="create-api-page-container">
-    <div class="spacing"></div>
     <div class="create-api-container">
       <div class="heading">CREATE API GRAPH</div>
       <div class="content">
-        <input class="red-input" type="Text" placeholder="PROJECT NAME" v-model="graph.projectName"/>
-        <input class="red-input" type="Text" placeholder="API NAME" v-model="graph.graphName"/>
+        <div class="nav-actions left">
+          <input class="red-input" type="Text" placeholder="PROJECT NAME" v-model="graph.projectName"/>
+          <input class="red-input" type="Text" placeholder="API NAME" v-model="graph.graphName"/>
 
-        <div class="nav-actions">
-          <div class="button button-red"> Test </div>
-          <div class="button button-green"> Save </div>
-          <div class="button button-blue"> Add Node </div>
-          <div class="button button-blue"> Actions </div>
+
+          <div class="btn btn-primary"> Test </div>
+          <div class="btn btn-danger" @click="printGraph()"> Save </div>
+
+
         </div>
+        <div class="nav-actions right">
+          <b-dropdown id="ddown1" text="Add Node" right class="m-md-2" variant="success" >
+                    <b-dropdown-item v-for="node in availableNodes">
+                      {{node}}
+                    </b-dropdown-item>
+                  </b-dropdown>
+        </div>
+
 
         <div class="node-container">
           <div class="node-list">
             <div class="heading">LIST OF NODES  </div>
             <ul>
               <li class="node" v-for="(node) in graph.nodeMap" :key="node.nodeName" v-bind:class="{selected: nodeSeen === 'node-'+node.nodeName}" @click="nodeSeen = 'node-'+node.nodeName">
-                {{key}}
+                {{node.nodeName}}
               </li>
             </ul>
           </div>
           <div class="spacing"></div>
 
           <div class="node-details" v-for="(node) in graph.nodeMap" :key="node.nodeName">
-            <template v-if="nodeSeen === 'node-'+key">
+            <template v-if="nodeSeen === 'node-'+node.nodeName">
               <div class="heading">
                 <div class="node-name">{{node.nodeName}}</div>
                 <div class="node-type">Type:  {{node.nodeClassName}}</div>
@@ -41,13 +50,13 @@
                 <span class="node-spec"> <a href="#">Fan out</a></span>
               </div>
               <div class="add-parameter-container">
-                <div id="add-parameter" class="button button-blue" @click="addParam(node)"> Add Parameter </div>
+                <div id="add-parameter" class="btn btn-primary" @click="addParam(node)"> Add Parameter </div>
               </div>
               <div class="parameter-container">
                 <div class="param-list-container">
                   <ul>
                     <li class="node"  v-for="(parameter) in node.parameterMap" :key="parameter.parameterName" v-bind:class="{selected: paramSeen === 'node-'+node.nodeName+'-param-'+ parameter.parameterName}" @click="paramSeen = 'node-'+node.nodeName+'-param-'+parameter.parameterName">
-                      {{key}}
+                      {{parameter.parameterName}}
                     </li>
                   </ul>
                 </div>
@@ -76,7 +85,6 @@
         </div>
       </div>
     </div>
-    <div class="spacing"></div>
   </div>
 </template>
 
@@ -89,7 +97,8 @@ export default {
       graph: JSON.parse(' { "projectName": "TestProject", "graphName": "TestGraph", "nodeMap": { "Node1": { "nodeName": "Node1", "nodeClassName": "com.atom8.piedpiper.node.mock.MockNode1" }, "Node2": { "nodeName": "Node2", "nodeClassName": "com.atom8.piedpiper.node.mock.MockNode2" }, "Node3": { "nodeName": "Node3", "nodeClassName": "com.atom8.piedpiper.node.mock.MockNode3", "parameterMap": { "param1": { "parameterName": "param1", "parameterType": "CONSTANT", "parameterValue": "akash" }, "param2": { "parameterName": "param2", "parameterType": "VALUE_SPECIFIED_AT_RUNTIME", "parameterValue": "runtimeValue" }, "param3": { "parameterName": "param3", "parameterType": "REFERENCE_FROM_ANOTHER_NODE", "parameterValue": "$.referenceValue1", "referenceNodeName": "Node1" }, "param4": { "parameterName": "param4", "parameterType": "REFERENCE_FROM_ANOTHER_NODE", "parameterValue": "$.referenceValue2", "referenceNodeName": "Node2" } } }, "Node4": { "nodeName": "Node4", "nodeClassName": "com.atom8.piedpiper.node.mock.MockNode4", "parameterMap": { "param1": { "parameterName": "param1", "parameterType": "REFERENCE_FROM_ANOTHER_NODE", "parameterValue": "$.referenceValue3", "referenceNodeName": "Node3" } } }, "Node5": { "nodeName": "Node5", "nodeClassName": "com.atom8.piedpiper.node.mock.MockNode5", "parameterMap": { "param1": { "parameterName": "param1", "parameterType": "REFERENCE_FROM_ANOTHER_NODE", "parameterValue": "$.referenceValue3", "referenceNodeName": "Node3" } } }, "Node6": { "nodeName": "Node6", "nodeClassName": "com.atom8.piedpiper.node.mock.MockNode6", "parameterMap": { "param1": { "parameterName": "param1", "parameterType": "REFERENCE_FROM_ANOTHER_NODE", "parameterValue": "$.referenceValue3", "referenceNodeName": "Node3" } } }, "Node7": { "nodeName": "Node7", "nodeClassName": "com.atom8.piedpiper.node.mock.MockNode7", "parameterMap": { "param1": { "parameterName": "param1", "parameterType": "REFERENCE_FROM_ANOTHER_NODE", "parameterValue": "$.referenceValue4", "referenceNodeName": "Node4" }, "param2": { "parameterName": "param2", "parameterType": "REFERENCE_FROM_ANOTHER_NODE", "parameterValue": "$.referenceValue5", "referenceNodeName": "Node5" }, "param3": { "parameterName": "param3", "parameterType": "REFERENCE_FROM_ANOTHER_NODE", "parameterValue": "$.referenceValue6", "referenceNodeName": "Node6" } } } } }'),
       parameterTypes: ['CONSTANT', 'REFERENCE_FROM_ANOTHER_NODE', 'VALUE_SPECIFIED_AT_RUNTIME'],
       paramSeen: '',
-      nodeSeen: ''
+      nodeSeen: '',
+      availableNodes: ['DynamoDBRetrieverNode', 'RESTServiceNode']
     }
   },
   methods: {
@@ -99,8 +108,11 @@ export default {
       }
       var paramMap = node.parameterMap
       var paramName = 'param' + (parseInt(Object.keys(paramMap).length) + 1)
-      var newParameter = { 'parameterName': paramName, 'parameterType': '', 'parameterValue': '', 'referenceNodeName': '' }
+      var newParameter = { 'parameterName': paramName, 'parameterType': 'CONSTANT', 'parameterValue': '', 'referenceNodeName': '' }
       this.$set(paramMap, paramName, newParameter)
+    },
+    printGraph: function () {
+      alert(JSON.stringify(this.graph))
     }
   }
 }
